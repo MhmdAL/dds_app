5e8c5450d7d85dda438f581475f300e669767768
Mohamad Alnass <alnass@snoonu.com>
2022-02-19 00:05:41 +0300

    added support for logout..

--- app/src/main/java/com/example/dds/MainActivity.kt
+++ app/src/main/java/com/example/dds/MainActivity.kt
@@ -1,15 +1,37 @@
 package com.example.dds
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_overflow, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_sign_out -> {
+                Firebase.auth.signOut()
+
+                navController.navigate(R.id.action_trackMissionFragment_to_loginFragment)
+            }
+        }
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +42,10 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(
             R.id.fragmentContainerView
         ) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
 
-//        val appBarConfiguration = AppBarConfiguration(topLevelDestinationIds = setOf(R.id.loginFragment, R.id.createMissionFragment))
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+//        val appBarConfiguration = AppBarConfiguration(topLevelDestinationIds = setOf(R.id.loginFragment, R.id.trackMissionFragment))
 //        val toolbar = findViewById<Toolbar>(R.id.toolbar)
 //
 //        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration)
